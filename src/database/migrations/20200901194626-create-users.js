5c11a633b531a719372fcbd93be348e7081c29ee
Luiz Silva <luizsilva.ee@gmail.com>
2020-09-01 17:02:56 -0300

    Migration de usuário admin

---
+++ src/database/migrations/20200901194626-create-users.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        alowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        alowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        alowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        alowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        alowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        alowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('users');
+  },
+};
