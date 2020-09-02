58de611653b4e51e313ff2b055f5d59625437c5f
Luiz Silva <luizsilva.ee@gmail.com>
2020-09-02 01:21:32 -0300

    Adicionado middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.post('/users', StudentController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+// Todas as rotas abaixo vão precisar estar autenticadas
+// (inserir token de autenticação no auth bearer com o token gerado na sessão)
+
+routes.post('/users', StudentController.store);
+routes.put('/users', StudentController.update);
 
 export default routes;
