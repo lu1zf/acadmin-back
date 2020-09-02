32145320a6b09e489ff8033a8f5d2dc1225e6a7a
Luiz Silva <luizsilva.ee@gmail.com>
2020-09-02 00:13:17 -0300

    Criado controller de student/atualizado model

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,25 @@
+﻿import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    const { id, name, email, weight, height } = await Student.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
